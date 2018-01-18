0ea6e40e4cd8436a2a9982dc84f0c818eb7e25d8
danikulin <nikos-dima@mail.ru>
2018-01-18 12:17:00 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,18 @@
 package ru.job4j;
 
+/**
+ * Calculate.
+ *
+ * @author Dmitriy Nikulin (nikos-dima@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
 	public class Calculate {
-		
+		/**
+     * Main.
+	 * Вывод строки в консоль.
+     * @param args - args.
+     */
 		public static void main (String [] args) {
 			System.out.println ("Hello, World!");
 		}  
---
+++ chapter_001/src/main/java/ru/job4j/Package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Dmitriy Nikulin (nikos-dima@mail.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
