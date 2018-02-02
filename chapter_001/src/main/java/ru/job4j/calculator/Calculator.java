8af23f5e7aeb8c95e4a225ee1870e296404be814
danikulin <nikos-dima@mail.ru>
2018-02-02 11:38:16 +0300

    2.3. Элементарный калькулятор. [#185]

---
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,51 @@
+package ru.job4j.calculator;
+/**
+ * Calculator.
+ *
+ * @author Dmitriy Nikulin (nikos-dima@mail.ru)
+ * @version 1
+ * @since 30.01.2018
+ */
+public class Calculator {
+    private double result;
+
+    /**
+     * Метод сложения.
+     * @param first первый аргумент.
+     * @param second второй аргумент.
+     */
+    public void add(double first, double second) {
+        this.result = first + second;
+    }
+    /**
+     * Метод вычетания.
+     * @param first первый аргумент.
+     * @param second второй аргумент.
+     */
+    public void subtract(double first, double second) {
+        this.result = first - second;
+    }
+    /**
+     * Метод деления.
+     * @param first первый аргумент.
+     * @param second второй аргумент.
+     */
+    public void div(double first, double second) {
+        this.result = first / second;
+    }
+    /**
+     * Метод умножения.
+     * @param first первый аргумент.
+     * @param second второй аргумент.
+     */
+    public void multiple(double first, double second) {
+        this.result = first * second;
+    }
+    /**
+     * Метод getResult.
+     * @return результат.
+     */
+    public double getResult() {
+        return this.result;
+    }
+}
