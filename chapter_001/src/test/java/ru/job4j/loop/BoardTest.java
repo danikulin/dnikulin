67dd3fd10b6d2a424b1cb68a437e580bc0e5fd32
danikulin <nikos-dima@mail.ru>
2018-02-16 00:05:04 +0300

    4.2. Создать программу вычисляющую факториал. [#193]

---
+++ chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+ * Test.
+ *
+ * @author Dmitriy Nikulin (nikos-dima@mail.ru)
+ * @version 1
+ * @since 15.02.2018
+ */
+public class BoardTest {
+    /**
+     * Test paint.
+     * Проверяет построение шахматной доски размером 3 на 3.
+     */
+    @Test
+    public void whenPaintBoardWithWidthThreeAndHeightThreeThenStringWithThreeColsAndThreeRows() {
+        Board board = new Board();
+        String result = board.paint(3, 3);
+        final String line = System.getProperty("line.separator");
+        String expected = String.format("x x%s x %sx x%s", line, line, line);
+        assertThat(result, is(expected));
+    }
+    /**
+     * Test paint2.
+     * Проверяет построение шахматной доски размером 5 на 4.
+     */
+    @Test
+    public void whenPaintBoardWithWidthFiveAndHeightFourThenStringWithFiveColsAndFourRows() {
+        Board board = new Board();
+        String result = board.paint(5, 4);
+        final String line = System.getProperty("line.separator");
+        String expected = String.format("x x x%s x x %sx x x%s x x %s", line, line, line, line);
+        assertThat(result, is(expected));
+    }
+}
