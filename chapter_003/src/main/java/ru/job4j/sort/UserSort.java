30c6f12bcf87f64b50c024429f38ac73b7e702e9
danikulin <nikos-dima@mail.ru>
2018-09-24 17:57:45 +0300

    Сортировка промежуточня.

--- chapter_003/src/main/java/ru/job4j/sort/UserSort.java
+++ chapter_003/src/main/java/ru/job4j/sort/UserSort.java
@@ -6,6 +6,8 @@ package ru.job4j.sort;
  * @version 1.1
  * @since 24.09.2018
  */
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.TreeSet;
 
@@ -17,8 +19,17 @@ public class UserSort {
      * @return TreeSet User отсортированных в порядке возростания по возрасту.
      */
     public TreeSet<User> sort(List<User> users) {
-        TreeSet<User> result = new TreeSet<User>();
-        result.addAll(users);
-        return result;
+        return new TreeSet<User>(users);
+    }
+    public class SortNameLengts implements Comparator<User> {
+
+        @Override
+        public int compare(User o1, User o2) {
+            return o1.getName().compareTo(o2.getName());
+        }
+    }
+    public void sortNameLengts(List<User> users) {
+        Comparator<User> snl = new SortNameLengts();
+        return new ArrayList<User>(users).sort(snl);
     }
 }
