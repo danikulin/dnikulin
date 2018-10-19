5cd595cdea91f915467623e2c132d8151ebe54a4
danikulin <nikos-dima@mail.ru>
2018-10-19 20:26:06 +0300

    Банковские переводы. [#10038]

--- chapter_003/src/main/java/ru/job4j/bank/Bank.java
+++ chapter_003/src/main/java/ru/job4j/bank/Bank.java
@@ -1,6 +1,7 @@
 package ru.job4j.bank;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.TreeMap;
 
 /**
@@ -11,24 +12,43 @@ import java.util.TreeMap;
  * @since 08.10.2018
  */
 public class Bank {
-
-    private TreeMap<User, ArrayList<Account>> treemap = new TreeMap<>();
+    /**
+     * Хранилище пользователей и их аккаунтов.
+     */
+    private TreeMap<User, ArrayList<Account>> bank = new TreeMap<>();
 
     /**
      * Метод addUser.
-     *Добавляет нового пользователя в мап хранилище.
+     * Добавляет нового пользователя в мап хранилище.
      * @param user пользователь которого надо добавить.
      */
     public void addUser(User user) {
-        this.treemap.put(user, new ArrayList<>());
+        this.bank.putIfAbsent(user, new ArrayList<>());
     }
 
+    /**
+     * Метод deleteUser.
+     * Удоляет пользователя из хранилища.
+     * @param user пользователь которого надо удалить.
+     */
     public void deleteUser(User user) {
-        this.treemap.remove(user);
+        this.bank.remove(user);
     }
 
+    /**
+     * Метод addAccountToUser.
+     * Добавляет аккаунт юзеру.
+     * @param passport паспортные данные юзера, которому нужно добавить аккаунт.
+     * @param account аккаунт который надо добавить.
+     */
     public void addAccountToUser(String passport, Account account) {
-        this.treemap.get(user).add(account);
+        Set<User> user = this.bank.navigableKeySet();
+        for (User u : user) {
+            if (u.getPassport().equals(passport)) {
+                this.bank.get(u).add(account);
+                break;
+            }
+        }
     }
 
     //private Account getActualAccount(User user, Account account) {
@@ -36,17 +56,52 @@ public class Bank {
    //     return list.get(list.indexOf(account));
    // }
 
+    /**
+     * Метод deleteAccountFromUser.
+     * Удоляет аккаунт у юзера.
+     * @param passport паспортные данные юзера, у которого нужно удалить аккаунт.
+     * @param account аккаунт который нужно удолить.
+     */
     public void deleteAccountFromUser(String passport, Account account) {
-        this.treemap.get(user).remove(account);
+        Set<User> user = this.bank.navigableKeySet();
+        for (User u : user) {
+            if (u.getPassport().equals(passport)) {
+                this.bank.get(u).remove(account);
+                break;
+            }
+        }
     }
 
+    /**
+     * Метод getUserAccounts.
+     * Возвращает список аккаунтов пользователя.
+     * @param passport паспортные данные пользователя.
+     */
     public List<Account> getUserAccounts(String passport) {
-        return this.treemap.get(user);
+        List<Account> list = new ArrayList<>();
+        Set<User> user = this.bank.navigableKeySet();
+        for (User u : user) {
+            if (u.getPassport().equals(passport)) {
+                list = this.bank.get(u);
+                break;
+            }
+        }
+        return list;
     }
 
+    /**
+     * Метод transferMany.
+     * Переводит деньги между счетами одного пользователя или на счет другому пользователю.
+     * @param srcPassport паспортные данные пользователя который переводит деньги.
+     * @param srcRequisites реквезиты аккаунта с которого переводятся деньги.
+     * @param destPassport паспортные данные пользователя, которому переводятся деньги.
+     * @param destRequisites реквезиты аккаунта на который переводятся деньги.
+     * @param amount количество денег, которое надо перевести.
+     * @return truth - если перевод выполнен успешно, false - если перевод не выполнен.
+     */
     public boolean transferMany(String srcPassport, String srcRequisites, String destPassport, String destRequisites, double amount) {
-        return this.treemap.get(user1).contains(account1)
-                && this.treemap.get(user2).contains(account2)
+        return this.bank.get(user1).contains(account1)
+                && this.bank.get(user2).contains(account2)
                 && getActualAccount(user1, account1).transfer(
                 getActualAccount(user2, account2), amount);
     }
